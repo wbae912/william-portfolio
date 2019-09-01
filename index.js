856d46ca9c039b7b65ec0677694ce912374f54be
William Bae <wbae912@gmail.com>
2019-08-31 22:08:15 -0700

    added auto scroll. finished mobile styling

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('.hamburger-a').on('click', function(event) {
+    event.preventDefault();
+    let linkHref = $(this).attr('href');
+    let navHeight = $('nav').outerHeight();
+   
+
+    $('html, body').animate({
+      scrollTop: $(linkHref).offset().top - navHeight
+    }, 1000);
+  });
+});
