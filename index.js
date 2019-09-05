fc9f49437dcd6b10233ea0cf07b5e3283a530f52
William Bae <wbae912@gmail.com>
2019-09-04 19:00:39 -0700

    minor updates. cleaning up final code

--- index.js
+++ index.js
@@ -3,21 +3,17 @@ $(document).ready(function() {
     event.preventDefault();
     let linkHref = $(this).attr('href');
     let navHeight = $('nav').outerHeight();
-   
-
     $('html, body').animate({
       scrollTop: $(linkHref).offset().top - navHeight
     }, 500);
   });
+
   $('.nav-links').on('click', function(event) {
     event.preventDefault();
     let linkHref = $(this).attr('href');
     let navHeight = $('nav').outerHeight();
-   
-
     $('html, body').animate({
       scrollTop: $(linkHref).offset().top - navHeight
     }, 500);
   });
-
 });
